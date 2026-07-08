372b35aaf1b547d25bd6922182afe702609c0818
AitezazAhmed <stoneghold4103@gmail.com>
2026-07-08 13:32:53 +0500

    04_Input&Output.py

---
+++ 04_Input&Output.py
@@ -0,0 +1,29 @@
+############ output ########### 
+
+print("Hello World")
+
+#  output: Hello World
+
+print(10) 
+# output: 10
+
+
+name = "Aitezaz"
+age = 22
+
+print(name, age)
+ 
+# output:  Aitezaz 22   # Notice that Python automatically adds a space between the values.
+
+
+#  The sep Parameter 
+
+print("Python", "Java", "C++", sep="-")
+
+
+# output: Python-Java-C++
+
+############# input ##########
+name = input("Enter your name: ")  # input
+
+print(name)   # output
